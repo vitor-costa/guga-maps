97368309dee4d764bb0af5170cb838acbd08ad4f
Vitor Costa <vitor.costa@corp.globo.com>
2019-06-28 15:51:05 -0300

    Versionate static files

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, render_template
 # set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path='/static', template_folder='static')
+app._version = ""
 
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    if not app._version:
+        app._version = os.environ.get("VERSION", "")
+    return render_template('index.html', version=app._version)
 
 
 @app.route('/static/<path:filename>')
