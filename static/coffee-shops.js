6c5303c08b1e0b88ae88ae068aee929b55b74a63
Vitor Costa <vitor.costa@corp.globo.com>
2019-06-28 12:06:59 -0300

    Add new coffee shops

--- static/coffee-shops.js
+++ static/coffee-shops.js
@@ -2,7 +2,7 @@ coffeeshops_callback({"coffee_shops": [
 	{
 		"name": "Coffee Five",
 		"coordinates": [-22.902388, -43.176758],
-		"description": "Ótima cafeteria no centro do Rio!",
+		"description": "Ótima cafeteria no centro do Rio! Aqui você pode escolher sem medo entre espresso, coado ou cappuccino. Peça uma latte art nas bebidas com leite!",
 		"maps_link": "https://goo.gl/maps/LEBRGSVFv89MZ6Gy9",
 		"score": 3
 	},
@@ -30,14 +30,14 @@ coffeeshops_callback({"coffee_shops": [
 	{
 		"name": "Slow Bakery",
 		"coordinates": [-22.956198, -43.189537],
-		"description": "Uma padaria onde também se toma café",
+		"description": "Uma padaria onde também se toma café. Costumam ter pelo menos duas opções de café. Só fazem café coado.",
 		"maps_link": "https://goo.gl/maps/EUYTjtaUF7pB8BRs7",
 		"score": 2
 	},
 	{
 		"name": "Colab",
 		"coordinates": [-22.955294, -43.182216],
-		"description": "Espaço com comida, drinks e café",
+		"description": "Espaço com comida, drinks e café. Torrefação própria.",
 		"maps_link": "https://goo.gl/maps/JSdRpXAk6Be4chjo8",
 		"score": 2
 	},
@@ -47,5 +47,33 @@ coffeeshops_callback({"coffee_shops": [
 		"description": "Cafeteria e comidas",
 		"maps_link": "https://goo.gl/maps/VbNij15Z7rSCjNHR6",
 		"score": 0
+	},
+	{
+		"name": "Coffeetown Barra",
+		"coordinates": [-22.997569, -43.358095],
+		"description": "Boas opções de doces. Muitas opções de café para levar pra casa.",
+		"maps_link": "https://goo.gl/maps/Ckb5eHKpGpjrVpEz5",
+		"score": 1
+	},
+	{
+		"name": "Beco do Café",
+		"coordinates": [-23.002738, -43.321880],
+		"description": "Cafeteria com boas opções de café visitante",
+		"maps_link": "https://goo.gl/maps/HxE5YWgnNUJMir4WA",
+		"score": 1
+	},
+	{
+		"name": "Café Ao Leu",
+		"coordinates": [-22.979706, -43.190540],
+		"description": "Muitas opções de café, sempre com torras frescas e com duas opções de café para espresso. Também não se pode deixar de tomar um coado.",
+		"maps_link": "https://goo.gl/maps/xeG4hzP72eGebgNS6",
+		"score": 3
+	},
+	{
+		"name": "Sofá Café",
+		"coordinates": [-22.966132, -43.178959],
+		"description": "Espresso, coados e lanchinhos.",
+		"maps_link": "https://goo.gl/maps/p24kzixVcxpRqQFBA",
+		"score": 1
 	},
 ]});
